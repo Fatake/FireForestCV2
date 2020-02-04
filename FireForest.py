ea37245fe5c76c720cbdd1fddd241c6b1f70129b
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-04 12:18:36 -0600

    procesar atributos e imagenes

--- FireForest.py
+++ FireForest.py
@@ -1,7 +1,20 @@
 import args
 import sys
+import cv2
+import numpy as np
+
+#
+# Constantes
+#
+GRIND_COLUMS = 8
+GRIND_ROWS = 4
 
 def procesarArchivo():
+    '''
+        Abre el archivo recivido del argumento -f, --file
+        lo abre y lee todas las lineas y las retorna
+        en una lista de listas de strings
+    '''
     #Recibe los argumentos de la linea de comandos
     nombreArchivo = args.argumentos()
 
@@ -25,11 +38,33 @@ def procesarArchivo():
 #
 def main():
     lineas = procesarArchivo()
+    listaImg = []
+    '''
+    donde:
+    listaImg[0] = String nombre
+    listaImg[0] = imagen cv2
+    listaImg[0] = Atributo Clasificador(H,N,I.....)
+    '''
+    listaAc = []
+    # imagen
     for linea in lineas:
         # Si inicia una nueva imagen
-        if linea.find('img') != -1:
-            print(linea)
+        if linea.find('img') != -1:##Si encuentra un nombre de imagen
+            listaAc = []
+            nameImg = linea.replace("\n","")
+            nameImg = cv2.imread(nameImg)
+            listaImg.append(nameImg)
+            listaImg.append(nameImg)
+            cv2.imshow(nameImg,imagen)
+            cv2.waitKey(1000)
+
     print("\n\n")
+    print("Aqui")
+    
+
+    #Tabaja en milsegundos
+    #cv2.waitKey(0)#= es cualquier tecla
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
