f7422acec09b83517b7ca2fa5419ec0cd8be50a5
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-04 11:20:36 -0600

    Argumentos add

---
+++ args.py
@@ -0,0 +1,36 @@
+import argparse
+import sys
+#
+# Funcion que recibe los argumentos de la linea de comandos
+#
+def argumentos():
+    '''
+    Argumentos() recibe de la linea de comandos
+    -h, --help ayuda de los comandos
+    -f, --file nombre de alrchivo .cfg
+    --version  version del programa
+    Retorna los argumentos recibidos
+    '''
+    parser = argparse.ArgumentParser(description='FireForest')
+
+    parser.add_argument('-f','--file', 
+                    action='store',
+                    default=None,
+                    nargs=1,
+                    dest='archivoImg',
+                    help='Carga las imagenes a leer')
+
+    parser.add_argument('--version', action='version',
+                    version='%(prog)s 1.0')
+
+    args = parser.parse_args()
+
+    #Reviza si recibio los archivos
+    if args.archivoImg == None:
+        sys.exit("No a ingresado ningun archivo\nUtilice [-h] para ayuda")
+
+    #Retgorna un string con el nombre del archivo
+    return ""+args.archivoImg[0]
+
+if __name__ == '__main__':
+    sys.exit("Error, ejecute python program -h")
